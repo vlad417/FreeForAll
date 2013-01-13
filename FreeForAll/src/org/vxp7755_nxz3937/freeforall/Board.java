0793bbf4afc017c93b529f81f6024e041e85457f
Neil Zimmerman <nxz3937@gmail.com>
2013-01-13 13:54:02 -0500

    added shit to model

--- FreeForAll/src/org/vxp7755_nxz3937/freeforall/Board.java
+++ FreeForAll/src/org/vxp7755_nxz3937/freeforall/Board.java
@@ -2,12 +2,17 @@ package org.vxp7755_nxz3937.freeforall;
 
 public class Board {
 	
+	private int boardWidth;
+	private int boardHeight;
 	private PieceThread boardCells[][];
 	private int scores[] = {0,0,0,0};
 	
-	Board()
+	Board( int width, int height)
 	{
 		// init boardCells
+		this.boardWidth = width;
+		this.boardHeight = height;
+		boardCells = new PieceThread[this.boardWidth][this.boardHeight];
 	}
 	
 	public PieceThread getCell( int x, int y )
@@ -15,15 +20,46 @@ public class Board {
 		return null;
 	}
 	
-	public void setCell( int x, int y, PieceThread piece )
+	/**
+	 * Place the provided Piece Thread in the specified position
+	 * 
+	 * @param x		horizontal position to set piece, left to right
+	 * @param y		vertical position to set piece, top to bottom
+	 * @param piece PieceThread to set in specified position, provide null to
+	 * 				clear cell
+	 * @return true if piece was place in valid position, otherwise false
+	 */
+	public boolean setCell( int x, int y, PieceThread piece )
 	{
-		
+		this.boardCells[x][y] = piece;
 	}
 	
-	public void givePoint( int team )
+	/**
+	 * increments the score of the specified team
+	 * 
+	 * @param team		 the team number who's score should be incremented,
+	 * 					 accepts 1 - 4.
+	 * @return  the newly incremented score for the specified team, 
+	 * 			returns -1 if an invalid team # was provided
+	 */
+	public int givePoint( int team )
 	{
-		
+		// check team number
+		if ( team < 1 || team > 4 ) {
+			// return -1 if invalid team
+			return -1;
+		} else {
+			// otherwise, increment and return new score
+			int pos = team--;
+			this.scores[pos]++;
+			return this.scores[pos];
+		}
 	}
 	
-	public int[] getScores() { return scores; }
+	/**
+	 * Retrieve the array of scores for each team
+	 * 
+	 *  @return array of team scores ints, i.e. {team1, team2, team3, team4}
+	 */
+	public int[] getScores() { return this.scores; }
 }
